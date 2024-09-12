e70d37284045cfbb5a618d5d6fc30b4255d7c017
miguel.mendoza <miguel.mendoza@auleseebe.upc.edu>
2024-09-12 11:42:45 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,28 @@
 #Codigo para problema 1
 
-plot(1:5,col='red',pch=18)
+mis_dades <- mtcars
+ 
+mean(mis_dades$qsec) #Media (Promedio), mitjana
+hist(mis_dades$qsec, breaks = 15)
+cut(mis_dades$qsec,9) #Recortar en x intervalos
+table(cut(mis_dades$qsec,9)) #Frecuencia absoluta (ni)
+table(cut(mis_dades$qsec,9))/32 #fi
+
+freq <- table(cut(mis_dades$qsec,9))
+freq
+tabla <- data.frame(ni=freq,
+                    fi=freq/32,
+                    Ni=cumsum(freq),
+                    Fi=cumsum(freq/32))
+tabla
+
+mis_dades$drat
+median(mis_dades$drat) #Mediana. Punto medio de observaciones
+hist(mis_dades$drat)
+sort(mis_dades$drat)
+
+quantile(mis_dades$drat)
+quantile(mis_dades$mpg, 0.18)
+IQR(mis_dades$cyl)
+sd(mis_dades$cyl) #Desviación típica (s)
+var(mis_dades$cyl) #Varianza (s^2)
